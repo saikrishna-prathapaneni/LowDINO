a3fe7f57bb59fe0e74ec4a41b94fad7ff9a88272
saikrishna-prathapaneni <saiprathapaneni23@gmail.com>
2023-05-11 01:46:05 -0400

    moved distrb and checkpoint to utils.py

--- custom/utils.py
+++ custom/utils.py
@@ -1,106 +1,93 @@
-
 import os
-import sys
-import time
-import math
-import random
-import datetime
-import subprocess
-from collections import defaultdict, deque
-
-import numpy as np
 import torch
-from torch import nn
+import sys
+import torch.nn as nn
 import torch.distributed as dist
-from PIL import ImageFilter, ImageOps
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from sklearn.metrics import accuracy_score
+import datetime
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from torchvision.datasets import ImageFolder
 
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
 
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
 
-class MetricLogger(object):
-    def __init__(self, delimiter="\t"):
-        self.meters = defaultdict(SmoothedValue)
-        self.delimiter = delimiter
+    __builtin__.print = print
 
-    def update(self, **kwargs):
-        for k, v in kwargs.items():
-            if isinstance(v, torch.Tensor):
-                v = v.item()
-            assert isinstance(v, (float, int))
-            self.meters[k].update(v)
+def init_distributed_mode(args):
 
-    def __getattr__(self, attr):
-        if attr in self.meters:
-            return self.meters[attr]
-        if attr in self.__dict__:
-            return self.__dict__[attr]
-        raise AttributeError("'{}' object has no attribute '{}'".format(
-            type(self).__name__, attr))
+    # launched with submitit on a slurm cluster
+    if 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+        args.world_size = os.environ['WORLD_SIZE']
+    # launched naively with `python main_dino.py`
+    # we manually add MASTER_ADDR and MASTER_PORT to env variables
+    elif torch.cuda.is_available():
+        print('Will run the code on one GPU.')
+        args.rank, args.gpu, args.world_size = 0, 0, 1
+        os.environ['MASTER_ADDR'] = '127.0.0.1'
+        os.environ['MASTER_PORT'] = '29500'
+    else:
+        print('Does not support training without GPU.')
+        sys.exit(1)
 
-    def __str__(self):
-        loss_str = []
-        for name, meter in self.meters.items():
-            loss_str.append(
-                "{}: {}".format(name, str(meter))
-            )
-        return self.delimiter.join(loss_str)
+    dist.init_process_group(
+        backend="nccl",
+        world_size=args.world_size,
+        rank=args.rank,
+    )
 
-    def synchronize_between_processes(self):
-        for meter in self.meters.values():
-            meter.synchronize_between_processes()
+    torch.cuda.set_device(args.gpu)
+    print('| distributed init (rank {})'.format(
+        args.rank), flush=True)
+    dist.barrier()
+    setup_for_distributed(args.rank == 0)
 
-    def add_meter(self, name, meter):
-        self.meters[name] = meter
+def has_batchnorms(model):
+    bn_types = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d, nn.SyncBatchNorm)
+    for name, module in model.named_modules():
+        if isinstance(module, bn_types):
+            return True
+    return False
 
-    def log_every(self, iterable, print_freq, header=None):
-        i = 0
-        if not header:
-            header = ''
-        start_time = time.time()
-        end = time.time()
-        iter_time = SmoothedValue(fmt='{avg:.6f}')
-        data_time = SmoothedValue(fmt='{avg:.6f}')
-        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
-        if torch.cuda.is_available():
-            log_msg = self.delimiter.join([
-                header,
-                '[{0' + space_fmt + '}/{1}]',
-                'eta: {eta}',
-                '{meters}',
-                'time: {time}',
-                'data: {data}',
-                'max mem: {memory:.0f}'
-            ])
-        else:
-            log_msg = self.delimiter.join([
-                header,
-                '[{0' + space_fmt + '}/{1}]',
-                'eta: {eta}',
-                '{meters}',
-                'time: {time}',
-                'data: {data}'
-            ])
-        MB = 1024.0 * 1024.0
-        for obj in iterable:
-            data_time.update(time.time() - end)
-            yield obj
-            iter_time.update(time.time() - end)
-            if i % print_freq == 0 or i == len(iterable) - 1:
-                eta_seconds = iter_time.global_avg * (len(iterable) - i)
-                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
-                if torch.cuda.is_available():
-                    print(log_msg.format(
-                        i, len(iterable), eta=eta_string,
-                        meters=str(self),
-                        time=str(iter_time), data=str(data_time),
-                        memory=torch.cuda.max_memory_allocated() / MB))
-                else:
-                    print(log_msg.format(
-                        i, len(iterable), eta=eta_string,
-                        meters=str(self),
-                        time=str(iter_time), data=str(data_time)))
-            i += 1
-            end = time.time()
-        total_time = time.time() - start_time
-        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print('{} Total time: {} ({:.6f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
+def save_checkpoint(checkpoint_dir, epoch, model, knn_acc,args, linear_acc=0, checkpoint_filename="student_model"):
+    now = datetime.datetime.now()
+    iteration_dir = now.strftime("%Y-%m-%d_%H-%M-%S")+'_epoch_'+str(epoch)
+    print(iteration_dir)
+    os.makedirs(os.path.join(checkpoint_dir, iteration_dir))
+
+    checkpoint_data = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        
+        'knn_accuracy': knn_acc,
+        'linear_acc':linear_acc
+    }
+
+    #print(checkpoint_data)
+    checkpoint_path = os.path.join(checkpoint_dir, iteration_dir, checkpoint_filename + "_epoch{}.pth".format(epoch))
+    torch.save(checkpoint_data, checkpoint_path)
+    print(checkpoint_path)
+    # Save the args and accuracy to a separate file
+    args_filename = os.path.join(checkpoint_dir, iteration_dir, "args.txt")
+    with open(args_filename, "w") as f:
+        for arg in vars(args):
+            f.write("{}: {}\n".format(arg, getattr(args, arg)))
+    accuracy_filename = os.path.join(checkpoint_dir, iteration_dir, "accuracy.txt")
+    print(accuracy_filename)
+    with open(accuracy_filename, "a") as f:
+        f.write("Epoch {}: accuracy = {}\n".format(epoch, knn_acc))
+        f.write("Epoch {}: accuracy = {}\n".format(epoch, linear_acc))
